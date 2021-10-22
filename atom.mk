7097092ad43a87fe1bd41e06c991a9332ffeef65
Alexander Sack <asac@pantacor.com>
2021-10-22 18:03:10 +0200

    add pantavisor atom.mk

---
+++ atom.mk
@@ -0,0 +1,20 @@
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := openssl
+LOCAL_DESCRIPTION := openssl module
+
+LOCAL_EXPORT_LDLIBS =
+
+LOCAL_AUTOTOOLS_MAKE_BUILD_ARGS :=
+LOCAL_AUTOTOOLS_MAKE_INSTALL_ARGS := lib=lib
+
+define LOCAL_AUTOTOOLS_CMD_CONFIGURE
+	cp -r $(PRIVATE_PATH)/*  $(call module-get-build-dir,openssl)/obj
+	sh -c "cd $(call module-get-build-dir,openssl)/obj; CC=gcc ./Configure --prefix=/usr --cross-compile-prefix=$(TARGET_CROSS) $(TARGET_ARCH_OPENSSL)"
+endef
+
+include $(BUILD_AUTOTOOLS)
+
